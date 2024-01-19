064cd2316c80b558f64f04fa9ba82662d5d6900a
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2024-01-19 15:26:08 +0100

    fixes event page

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,9 +1,15 @@
 "use client"
+
 import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://gqfjcwerdsrxisvccceu.supabase.co";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL or key is undefined');
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function Events() {
@@ -17,6 +23,7 @@ function Events() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSuccessful, setIsSuccessful] = useState(false);
   const [feedback, setFeedback] = useState('');
+  const [newBooking, setNewBooking] = useState(defaultBooking); // Initialize newBooking state
 
   useEffect(() => {
     if (feedback && !isSuccessful) {
@@ -26,14 +33,19 @@ function Events() {
 
   const handleInputChange = (field, value) => {
     // You can implement additional input cleaning logic here if needed
-    setFieldValue(field, value.trim());
+    setNewBooking((prevBooking) => ({
+      ...prevBooking,
+      [field]: value.trim(),
+    }));
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the form from submitting in the default way
+
     setIsSubmitting(true);
 
     try {
-      const { data, error } = await supabase.from('bookings').upsert([{ ...newBooking }]);
+      const { data, error } = await supabase.from('bookings').upsert([newBooking]);
 
       if (error) {
         console.error('Error adding new booking:', error.message);
@@ -53,61 +65,65 @@ function Events() {
   };
 
   return (
-    <div>
-      <h1>Booking Form</h1>
+    <div className="booking-form-container">
+    <h1 className="form-title">Booking Form</h1>
 
-      {feedback && (
-        <div className="feedback">
-          <p>{feedback}</p>
-        </div>
-      )}
+    {feedback && (
+      <div className={`feedback ${isSuccessful ? 'success' : 'error'}`}>
+        <p>{feedback}</p>
+      </div>
+    )}
 
-      {!isSuccessful && (
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="booking-name">Name</label>
+    {!isSuccessful && (
+      <form className="textarea textarea-bordered" onSubmit={handleSubmit}>
+        <div className="textarea textarea-bordered">
+          <label htmlFor="booking-name">Name:</label>
           <input
             type="text"
             id="booking-name"
-            placeholder="Enter name"
             value={newBooking.name}
             onChange={(e) => handleInputChange('name', e.target.value)}
           />
+        </div>
 
-          <label htmlFor="booking-email">Email</label>
+        <div className="textarea textarea-bordered">
+          <label htmlFor="booking-email">Email:</label>
           <input
             type="email"
             id="booking-email"
-            placeholder="Enter email"
             value={newBooking.email}
             onChange={(e) => handleInputChange('email', e.target.value)}
           />
+        </div>
 
-          <label htmlFor="booking-phone">Phone</label>
+        <div className="textarea textarea-bordered">
+          <label htmlFor="booking-phone">Phone:</label>
           <input
             type="tel"
             id="booking-phone"
-            placeholder="Enter phone"
             value={newBooking.phone}
             onChange={(e) => handleInputChange('phone', e.target.value)}
           />
+        </div>
 
-          <label htmlFor="booking-body">Body</label>
+        <div className="textarea textarea-bordered">
+          <label htmlFor="booking-body">Additional information</label>
           <textarea
             id="booking-body"
-            placeholder="Describe what you are reaching out for."
             value={newBooking.body}
             onChange={(e) => handleInputChange('body', e.target.value)}
           />
+        </div>
 
-          <div className="button-container">
-            <button type="submit" className="submit-button" disabled={isSubmitting}>
-              {isSubmitting ? 'Submitting...' : 'Submit'}
-            </button>
-          </div>
-        </form>
-      )}
-    </div>
-  );
+        <div className="button-container">
+          <button type="submit" className="submit-button" disabled={isSubmitting}>
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
+        </div>
+      </form>
+    )}
+  </div>
+);
 }
 
 export default Events;
